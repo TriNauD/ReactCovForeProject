e4646beb638bfcce500d9744b82de3ec63c11a00
TriNauD <17801202079@163.com>
2021-05-09 10:56:52 +0800

    Add navigation to forecast page's sub pages(create forecast & model manage). Formatted the code.

--- android/app/page/ForecastPage.js
+++ android/app/page/ForecastPage.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Button, Text, View} from 'react-native';
 
 export default class ForecastPage extends Component {
-    render(){
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text>Forecast Page</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Create Forecast Page</Text>
+        <Button
+          title="createFore"
+          onPress={() => this.props.navigation.navigate('CreateForecastPage')}>
+          btn
+        </Button>
+        <Text>Model Manage Page</Text>
+        <Button
+          title="modelManage"
+          onPress={() => this.props.navigation.navigate('ModelManagePage')}>
+          btn
+        </Button>
+      </View>
+    );
+  }
 }
